bf0eb72254aee9caea8a471df4524ffa0bd6cbc0
siddwaji <siddwaji@sheridancollege.ca>
2025-10-14 21:03:50 -0400

    Add multiply method to Calculator

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Calculator Application - Updated");
+    }
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
